7f87a9e915a72eb6ce496f631d2bae445dcfa86e
raythompsonwebdev <ray_thomp@hushmail.com>
2021-06-12 17:12:11 +0100

    need to merge branches

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,29 +1,45 @@
 //import logo from './logo.svg';
 import "./App.css";
 
-const Hello = () => {
+const Hello = props => {
   return (
     <div>
-      <p>Hello world</p>
+      {/* <p>Hello {props.name}</p> */}
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   );
 };
 
 const App = () => {
   //console.log('Hello from component')
-  const now = new Date();
-  const a = 10;
-  const b = 20;
+
+  // const now = new Date();
+  // const a = 10;
+  // const b = 20;
+
+  const name = "Peter";
+  const age = 10;
+
   return (
-    <div>
-      <p>Hello world, it is {now.toString()}</p>
+    //<div>
+    //Fragemnt- wrap elements to be returned by the component with an empty element
+    <>
+      {/* <p>Hello world, it is {now.toString()}</p>
       <p>
         {a} plus {b} is {a + b}
-      </p>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      </p> */}
+      {/* <Hello name="George" />
+      <Hello name="Daisy" />
+      <Hello name="Raymond" /> */}
+
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </>
+    //Fragemnt- wrap elements to be returned by the component with an empty element
+    //</div>
   );
 };
 
